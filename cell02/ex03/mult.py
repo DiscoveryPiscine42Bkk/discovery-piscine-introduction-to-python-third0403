cfec03a8c7aba93c12a2f3d7c45b5f47880cfbe3
third0403 <ramidabiscuit@gmail.com>
2025-06-18 00:27:44 +0700

    Update mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,1 +1,10 @@
-
+x = int(input("Enter the first number :\n"))
+y = int(input("Enter the second number :\n"))
+z = x * y
+print(f"{x} x {y} = {z}")
+if z > 0:
+    print("The result is positive")
+elif z < 0:
+    print("The result is negative")
+else :
+    print("The result is positive and negative.")
