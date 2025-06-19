29217e77ae05e1c5e9fbadecbcd5598f13a40ee3
third0403 <ramidabiscuit@gmail.com>
2025-06-19 23:45:44 +0700

    Update play_with_arrays.py

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,12 +1,12 @@
 x=  [2,8,9,48,8,22,-12,2]
-y= []
+y= set()
 
 
 for i in range(len(x)):
   
   if x[i] > 5:
     if x[i] + 2 not in y:
-       y.append(x[i] + 2)
+       y.add(x[i] + 2)
     
   
 print(x)
