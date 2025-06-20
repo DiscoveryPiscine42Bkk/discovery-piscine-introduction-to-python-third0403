abb44309e0049705a308dfabe4bb92b7d4e545ea
third0403 <ramidabiscuit@gmail.com>
2025-06-20 13:45:54 +0700

    Update string_are_arrays.py

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -1,1 +1,15 @@
+import sys
+def main():
+    if len(sys.argv) != 2:
+        print("none")
+        return
+    
+    text=sys.argv[1]
+    count_z=text.count('z')
 
+    if count_z == 0:
+        print("none")
+    else:
+
+        print('z'*count_z)
+main()
