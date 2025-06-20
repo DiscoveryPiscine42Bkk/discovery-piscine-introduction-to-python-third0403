1679afdd2b25f263b3fb17706cc99a378192dcac
third0403 <ramidabiscuit@gmail.com>
2025-06-20 13:45:06 +0700

    Update parameter_matching.py

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,1 +1,12 @@
-
+import sys
+def main():
+    if len(sys.argv)!=2:
+        print("none")
+        return
+    param=sys.argv[1]
+    answer=input("what was the paramenter?")
+    if answer==param:
+        print("Good job!")
+    else:
+        print("Nope,sorry...")
+main()
