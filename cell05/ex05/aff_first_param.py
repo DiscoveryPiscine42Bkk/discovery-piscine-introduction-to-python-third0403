b6d02492d403cc354613b953388cffbc04d5ac2b
third0403 <ramidabiscuit@gmail.com>
2025-06-20 13:42:09 +0700

    Update aff_first_param.py

--- cell05/ex05/aff_first_param.py
+++ cell05/ex05/aff_first_param.py
@@ -1,1 +1,11 @@
-
+import sys
+def main():
+    """
+    ฟังก์ชันหลักของโปรแกรมสำหรับแสดงพารามิเตอร์ตัวแรก
+    """
+    if len(sys.argv) > 1:
+        print(sys.argv[1])
+    else:
+        print("none")
+if __name__ == "__main__":
+    main()
