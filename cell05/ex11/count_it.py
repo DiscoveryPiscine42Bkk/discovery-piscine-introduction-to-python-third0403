4de3ff036f99d864ca5791dd9405be5869b98fa1
third0403 <ramidabiscuit@gmail.com>
2025-06-20 13:45:24 +0700

    Update count_it.py

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -1,1 +1,10 @@
-
+import sys
+def main():
+    params=sys.argv[1:]
+    if len(params)==0:
+        print("none")
+    else:
+        print(f"parameters:{len(params)}")
+        for p in params:
+            print(f"{p}:{len(p)}")
+main()
