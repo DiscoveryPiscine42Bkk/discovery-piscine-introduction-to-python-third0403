2d04744493f1305adfe614107f48fd3a6c352957
third0403 <ramidabiscuit@gmail.com>
2025-06-20 13:46:19 +0700

    Update append_it.py

--- cell05/ex13/append_it.py
+++ cell05/ex13/append_it.py
@@ -1,1 +1,13 @@
+import sys
 
+def main():
+    params=sys.argv[1:]
+    if len(params)==0:
+        print("none")
+        return
+    
+    for p in params:
+        if p.endswith("ism"):
+            continue
+        print(p+"ism")
+main()
