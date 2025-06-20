83596f7db3268eac01b654cb63b9b5a0e646202e
third0403 <ramidabiscuit@gmail.com>
2025-06-20 13:44:46 +0700

    Update scan_it.py

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,1 +1,13 @@
-
+import sys
+import re
+if len(sys.argv) !=3:
+    print("none")
+    sys.exit(0)
+keyword = sys.argv[1]
+text = sys.argv[2]
+pattern = f'(?={re.escape(keyword)})'
+matches = re.findall(pattern, text)
+if not matchs:
+    print("none")
+else:
+    print(len(matches))
